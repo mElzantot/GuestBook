495840409dbac347ec8ef3b8ed02786e8376ccb1
Mohamed Shaker <m.elzantot@gmail.com>
2022-08-15 16:49:13 +0200

    add service Registerations for repos and JWT

---
+++ GuestBook.BAL/ServicesRegisteration.cs
@@ -0,0 +1,56 @@
+﻿using GuestBook.DAL.Interfaces;
+using GuestBook.DAL.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBook.BAL
+{
+    public static class ServicesRegisteration
+    {
+        public static void AddInfraStructure(this IServiceCollection services)
+        {
+            services.AddTransient(typeof(IGuestRepository), typeof(GuestRepository))
+                    .AddTransient(typeof(IMessageRepository), typeof(MessageRepository));
+        }
+
+        public static void ConfigureJWTAuth(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+           .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, jwtBearerOptions =>
+           {
+               jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
+               {
+                   ValidateIssuerSigningKey = true,
+                   IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"])),
+                   ValidateIssuer = false,
+                   ValidateAudience = false,
+                   ValidateLifetime = true,
+                   ClockSkew = TimeSpan.Zero
+               };
+               jwtBearerOptions.Events = new JwtBearerEvents
+               {
+                   OnAuthenticationFailed = context =>
+                   {
+                       if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                       {
+                           context.Response.Headers.Add("Token-Expired", "true");
+                       }
+                       return Task.CompletedTask;
+                   }
+               };
+           });
+        }
+    }
+}
