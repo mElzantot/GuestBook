2c353fc045cdb7670a80bc7304f7cfd3af9a6f40
Mohamed Shaker <m.elzantot@gmail.com>
2022-08-15 11:48:19 +0200

    Add main entites [ Guest & Message ] classes

---
+++ GuestBook.DAL/Entites/Guest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBook.DAL.Entites
+{
+    public class Guest
+    {
+        public int Id { get; set; }
+        public string? GuestName { get; set; }
+        public string? PasswordHash { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpirationDate { get; set; }
+        public DateTime CreationDate { get; set; }
+
+    }
+}
---
+++ GuestBook.DAL/Entites/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBook.DAL.Entites
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string? MessageBody { get; set; }
+        public int GuestId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int? ParentMessageId { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
