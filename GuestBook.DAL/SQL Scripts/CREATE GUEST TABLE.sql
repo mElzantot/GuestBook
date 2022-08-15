24d241ffd3bcabdf169482957ba5f97023b5a446
Mohamed Shaker <m.elzantot@gmail.com>
2022-08-15 11:48:40 +0200

    Add create scripts for main entites

---
+++ GuestBook.DAL/SQL Scripts/CREATE GUEST TABLE.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Guest]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1) , 
+	[GuestName] nvarchar(20) Not Null UNIQUE ,
+	[PasswordHash] nvarchar(250) Not Null ,
+	[RefreshToken] nvarchar(250)  Null ,
+	[RefreshTokenExpirationDate] datetime2 NULL ,
+	[CreationDate] datetime2 not null
+)
