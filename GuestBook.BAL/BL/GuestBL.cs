c86fbeac21f957efb42f87cb6a3515526ace3ecf
Mohamed Shaker <m.elzantot@gmail.com>
2022-08-15 12:13:19 +0200

    add BL interfaces and Classes --W/O implemtation

---
+++ GuestBook.BAL/BL/GuestBL.cs
@@ -0,0 +1,13 @@
+﻿using GuestBook.BAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBook.BAL.BL
+{
+    internal class GuestBL : IGuestBL
+    {
+    }
+}
