4cae7349b4aa2b903eaff3e72890a02e5aa8c5e7
Mohamed Shaker <m.elzantot@gmail.com>
2022-08-15 11:58:41 +0200

    Add base repo interface with CRUD operations

---
+++ GuestBook.DAL/Interfaces/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBook.DAL.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<T> InsertAsync(T item);
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(object id);
+
+    }
+}
