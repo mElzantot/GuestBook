53d4627aacda279752a35c913d597ff4019b5f96
Mohamed Shaker <m.elzantot@gmail.com>
2022-08-15 15:44:13 +0200

    - Add Test Project -- Add Test Cases for Auth Validator

---
+++ GuestBook.Test/AuthValidatorTest.cs
@@ -0,0 +1,66 @@
+﻿using FluentValidation.TestHelper;
+using GuestBook.BAL.DTO;
+using GuestBook.BAL.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBook.Test
+{
+
+    public class AuthValidatorTest
+    {
+        private readonly AuthValidator _validator = new AuthValidator();
+
+
+        [Fact]
+        public void GiveAnEmptyName_ShouldHaveValidationError()
+        {
+            AuthRequestDTO authRequestDTO = new AuthRequestDTO
+            {
+                UserName = String.Empty,
+                Password = "Mohamed"
+            };
+            var result = _validator.TestValidate(authRequestDTO);
+            result.ShouldHaveValidationErrorFor(user => user.UserName);
+        }
+        [Fact]
+        public void GiveAnOneCharName_ShouldHaveValidationError()
+        {
+            AuthRequestDTO authRequestDTO = new AuthRequestDTO
+            {
+                UserName = "S",
+                Password = "Mohamed"
+            };
+            var result = _validator.TestValidate(authRequestDTO);
+            result.ShouldHaveValidationErrorFor(user => user.UserName);
+        }
+
+        [Fact]
+        public void GiveAnEmptyPassword_ShouldHaveValidationError()
+        {
+            AuthRequestDTO authRequestDTO = new AuthRequestDTO
+            {
+                UserName = "Mohamed",
+                Password = ""
+            };
+            var result = _validator.TestValidate(authRequestDTO);
+            result.ShouldHaveValidationErrorFor(user => user.Password);
+        }
+
+        [Fact]
+        public void GiveValidUSerNameAndPassword_ShouldNotHaveValidationErrors()
+        {
+            AuthRequestDTO authRequestDTO = new AuthRequestDTO
+            {
+                UserName = "Mohamed",
+                Password = "shaker"
+            };
+            var result = _validator.TestValidate(authRequestDTO);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+    }
+}
---
+++ GuestBook.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace GuestBook.Test
+{
+    public class GuestAuthTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
