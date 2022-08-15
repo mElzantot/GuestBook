beced8dcc92b1094c0eeb32b66d02648c77297a8
Mohamed Shaker <m.elzantot@gmail.com>
2022-08-15 15:11:36 +0200

    add message DTO for [Add & new ] commands

---
+++ GuestBook.BAL/DTO/UpdateMessageDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBook.BAL.DTO
+{
+    public class UpdateMessageDTO
+    {
+        public int Id { get; set; }
+        public string? MessageBody { get; set; }
+    }
+}
